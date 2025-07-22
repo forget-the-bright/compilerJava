840b94ec1494e1007e98cf445d61ae81955fbd40
wanghao <kshelloworldwh@gmail.com>
2025-07-22 14:11:47 +0800

    build: 更新 Docker 镜像并推送到仓库
    - 移除旧的 Docker 镜像
    - 构建新的 Docker镜像
    - 运行新的 Docker 容器
    - 删除旧的仓库镜像
    -将新镜像标记为仓库最新版本
    - 将新镜像推送到仓库

--- build.sh
+++ build.sh
@@ -4,3 +4,6 @@
    mvn clean package  -Dfile.encoding=UTF-8 -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Dmaven.wagon.http.ssl.ignore.validity.dates=true -U -Dmaven.test.skip=true
    docker build -t compiler-java-app .
    docker run -d -p 801:80 --name compiler-app compiler-java-app
+   docker rmi kskswanghao/compiler-java-app:latest
+   docker tag compiler-java-app kskswanghao/compiler-java-app:latest
+   docker push kskswanghao/compiler-java-app:latest
